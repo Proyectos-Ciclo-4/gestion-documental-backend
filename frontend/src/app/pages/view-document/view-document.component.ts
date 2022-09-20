20a600fb6d836ac2af04aa700143a8be362776e2
Leonardo henao <leonardofhenao.085@gmail.com>
2022-09-19 21:49:24 -0500

    [frontend]Estructura paginas modulares creadas
    navbar creado

---
+++ frontend/src/app/pages/view-document/view-document.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'document',
+  templateUrl: './view-document.component.html',
+  styleUrls: ['./view-document.component.css']
+})
+export class ViewDocumentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
