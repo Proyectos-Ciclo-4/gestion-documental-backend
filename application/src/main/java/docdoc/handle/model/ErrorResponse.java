b9c7f81778246a1a0d13ab9ee5e5f05322587faa
ACamilo <andresdm1997@gmail.com>
2022-09-23 12:42:08 -0500

    creando la respuesta al error

---
+++ application/src/main/java/docdoc/handle/model/ErrorResponse.java
@@ -0,0 +1,13 @@
+package docdoc.handle.model;
+
+public class ErrorResponse {
+    private final String message;
+
+    public ErrorResponse(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
